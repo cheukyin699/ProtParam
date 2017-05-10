4ab6d96e351fba30f4c55191f1662775001ae61b
Cheuk Yin Ng <cheukyin.ng@mytwu.ca>
2017-05-10 11:23:10 -0700

    Fixes some formatting issues

--- get_html_protParamValues_UserSeq.pl
+++ get_html_protParamValues_UserSeq.pl
@@ -50,84 +50,78 @@ my $protein_name = $_;
 
 my %seq = ();
 my $in  = Bio::SeqIO->new(-file => "$inputfile" ,
-                           -format => 'Fasta');
+    -format => 'Fasta');
 
-    while ( my $seq = $in->next_seq() ) {
-       $seq{$seq->id} = $seq->seq;    
-    }
-
-
-
-
+while ( my $seq = $in->next_seq() ) {
+    $seq{$seq->id} = $seq->seq;    
+}
 
 for my $id ( keys %seq){
-chomp $id;
-my $seq = $seq{$id};
+    chomp $id;
+    my $seq = $seq{$id};
 
-my $this_url = $url;
+    my $this_url = $url;
 #$this_url .= "sequence=$seq";
 
 # Makes a request to the URL
-my %formdata = 
-		(
-		"submit"=>"on",
-		"sequence"=>$seq,
-		);
-		
-my $request  = POST($this_url, [%formdata]);
+    my %formdata = 
+    (
+        "submit"=>"on",
+        "sequence"=>$seq,
+    );
+
+    my $request  = POST($this_url, [%formdata]);
 
 # ----> XML print out -------------------------------
-my $ua  = LWP::UserAgent->new(timeout=>2000);
-my $res = $ua->request($request);
+    my $ua  = LWP::UserAgent->new(timeout=>2000);
+    my $res = $ua->request($request);
 
-my $html = $res->as_string;
-  &pars_protParm_HTML( $id , $res->as_string);
+    my $html = $res->as_string;
+    &pars_protParm_HTML( $id , $res->as_string);
 }
 
 
 sub pars_protParm_HTML{
-	my $id = shift;
-	my $html = shift;
+    my $id = shift;
+    my $html = shift;
 
-	my %values = ();
+    my %values = ();
 
-	my @lines = split(/\n/,$html);
+    my @lines = split(/\n/,$html);
 
-	for my $line (@lines){
-		if($line =~ /Molecular weight\:\S+\s*(\d+.*\d*\b)/){
-			$values{'mw'} = $1;
-		}
-		elsif($line =~ /Theoretical pI\:\S+\s*(\d+.*\d*\b)/){
-			$values{'pI'} = $1;
-		}
-		elsif($line =~ /The instability index \(II\) is computed to be\s*([-]*\d+.*\d*\b)/){
-		    $values{'insta'} = $1;
-		}
-		elsif($line =~ /Aliphatic index\:\S+\s*(\d+.*\d*\b)/){
-			$values{'alipha'} = $1;
-		}
-		elsif($line =~ /Grand average of hydropathicity \(GRAVY\)\:\S+\s*([-]*\d+.*\d*\b)/){
-			$values{'hydro'} = $1;
-		}
-		elsif($line =~ /Total number of negatively charged residues \(Asp \+ Glu\):\S+\s*(\d+\b)/){
-			$values{'neg_charge'} = $1;
-		}
-		elsif($line =~ /Total number of positively charged residues \(Arg \+ Lys\):\S+\s*(\d+\b)/){
-			$values{'pos_charge'} = $1;
-		}
-	}
+    for my $line (@lines){
+        if($line =~ /Molecular weight\:\S+\s*(\d+.*\d*\b)/){
+            $values{'mw'} = $1;
+        }
+        elsif($line =~ /Theoretical pI\:\S+\s*(\d+.*\d*\b)/){
+            $values{'pI'} = $1;
+        }
+        elsif($line =~ /The instability index \(II\) is computed to be\s*([-]*\d+.*\d*\b)/){
+            $values{'insta'} = $1;
+        }
+        elsif($line =~ /Aliphatic index\:\S+\s*(\d+.*\d*\b)/){
+            $values{'alipha'} = $1;
+        }
+        elsif($line =~ /Grand average of hydropathicity \(GRAVY\)\:\S+\s*([-]*\d+.*\d*\b)/){
+            $values{'hydro'} = $1;
+        }
+        elsif($line =~ /Total number of negatively charged residues \(Asp \+ Glu\):\S+\s*(\d+\b)/){
+            $values{'neg_charge'} = $1;
+        }
+        elsif($line =~ /Total number of positively charged residues \(Arg \+ Lys\):\S+\s*(\d+\b)/){
+            $values{'pos_charge'} = $1;
+        }
+    }
 
-	my $charge = $values{'pos_charge'} - $values{'neg_charge'};
-	my @pri = (
-				$values{'mw'},
-				$charge,
-				$values{'pI'},
-				$values{'insta'},
-				$values{'alipha'},
-				$values{'hydro'});
-	print "$protein_name\t" . join("\t",@pri) ."\n";
-
+    my $charge = $values{'pos_charge'} - $values{'neg_charge'};
+    my @pri = (
+        $values{'mw'},
+        $charge,
+        $values{'pI'},
+        $values{'insta'},
+        $values{'alipha'},
+        $values{'hydro'});
+    print "$protein_name\t" . join("\t",@pri) ."\n";
 }
-
 
 __END__
